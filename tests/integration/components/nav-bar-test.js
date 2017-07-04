89fcbfceda00c86d7aac3c444ff247adaec058eb
Tim Cook <tc@papercloud.com.au>
2017-07-04 15:20:18 +1000

    added nav bar component

---
+++ tests/integration/components/nav-bar-test.js
@@ -0,0 +1,15 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('nav-bar', 'Integration | Component | nav bar', {
+  integration: true
+});
+
+test('it renders with logo, heading and current season', function(assert) {
+  assert.expect(3);
+
+  this.render(hbs`{{nav-bar}}`);
+  assert.ok(!!this.$("#nav-bar .left .logo img").length, 'should have logo image is present');
+  assert.equal(this.$("#nav-bar .left .heading").text().trim(), 'OpenFooty - Tagline', 'should have the open footy headline');
+  assert.equal(this.$("#nav-bar .right .item.season").text().trim(), 'Season 2017', 'should have current season');
+});
