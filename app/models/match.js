5818fce61592770d42b48913176116fe8bff2ce5
Tim Cook <tc@papercloud.com.au>
2017-07-06 13:55:27 +1000

    updated match model for completed

--- app/models/match.js
+++ app/models/match.js
@@ -1,5 +1,7 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
+const { computed } = Ember;
 const { Model, attr, belongsTo } = DS;
 
 export default Model.extend({
@@ -10,10 +12,62 @@ export default Model.extend({
 
   isCompleted: attr('boolean'),
 
+  homeScoreGoals: attr('number'),
+
+  homeScoreBehinds: attr('number'),
+
+  homeScoreTotal: attr('number'),
+
+  awayScoreGoals: attr('number'),
+
+  awayScoreBehinds: attr('number'),
+
+  awayScoreTotal: attr('number'),
+
+  // relationships
+
   homeTeam: belongsTo('team'),
 
   awayTeam: belongsTo('team'),
 
-  ground: belongsTo('ground')
+  ground: belongsTo('ground'),
+
+  // properties
+
+  didHomeTeamWin: computed('winningTeam.id', 'homeTeam.id', function() {
+    return this.get('winningTeam.id') === this.get('homeTeam.id');
+  }),
+
+  didAwayTeamWin: computed('winningTeam.id', 'awayTeam.id', function() {
+    return this.get('winningTeam.id') === this.get('awayTeam.id');
+  }),
+
+  didDraw: computed.equal('winningTeam', null),
+
+  winningTeam: computed('homeScoreTotal', 'awayScoreTotal', function() {
+    let winningTeam = null;
+
+    if (this.get('homeScoreTotal') > this.get('awayScoreTotal')) {
+      winningTeam = this.get('homeTeam');
+    } else if (this.get('homeScoreTotal') < this.get('awayScoreTotal')) {
+      winningTeam = this.get('awayTeam');
+    }
+
+    return winningTeam;
+  }),
+
+  prettyHomeScore: computed('homeScoreGoals', 'homeScoreBehinds', 'homeScoreTotal', function() {
+    return this.prettyScore(this.get('homeScoreGoals'), this.get('homeScoreBehinds'), this.get('homeScoreTotal'));
+  }),
+
+  prettyAwayScore: computed('awayScoreGoals', 'awayScoreBehinds', 'awayScoreTotal', function() {
+    return this.prettyScore(this.get('awayScoreGoals'), this.get('awayScoreBehinds'), this.get('awayScoreTotal'));
+  }),
+
+  // methods
+
+  prettyScore(goals, behinds, total) {
+    return `${goals}.${behinds} ${total}`;
+  }
 
 });
--- tests/unit/models/match-test.js
+++ tests/unit/models/match-test.js
@@ -5,8 +5,182 @@ moduleForModel('match', 'Unit | Model | match', {
   needs: ['model:team', 'model:ground']
 });
 
-test('it exists', function(assert) {
+test('it will return the home team as the winning team if the homeScoreTotal > awayScoreTotal', function(assert) {
+  let team1 = {
+    name: 'Essendon'
+  };
+  let team2 = {
+    name: 'Carlton'
+  };
+
+  let model = this.subject({
+    homeScoreTotal: 100,
+    awayScoreTotal: 80
+  });
+
+  model.homeTeam = team1;
+  model.awayTeam = team2;
+
+  assert.equal(model.get('winningTeam.name'), team1.name);
+});
+
+test('it will return the away team as the winning team if the homeScoreTotal < awayScoreTotal', function(assert) {
+  let team1 = {
+    name: 'Essendon'
+  };
+  let team2 = {
+    name: 'Carlton'
+  };
+
+  let model = this.subject({
+    homeScoreTotal: 80,
+    awayScoreTotal: 100
+  });
+
+  model.homeTeam = team1;
+  model.awayTeam = team2;
+
+  assert.equal(model.get('winningTeam.name'), team2.name);
+});
+
+test('it will null if the homeScoreTotal === awayScoreTotal', function(assert) {
+  let team1 = {
+    name: 'Essendon'
+  };
+  let team2 = {
+    name: 'Carlton'
+  };
+
+  let model = this.subject({
+    homeScoreTotal: 100,
+    awayScoreTotal: 100
+  });
+
+  model.homeTeam = team1;
+  model.awayTeam = team2;
+
+  assert.equal(model.get('winningTeam'), null);
+});
+
+test('it can check if the home team won', function(assert) {
   let model = this.subject();
-  // let store = this.store();
-  assert.ok(!!model);
+  let team1 = {
+    id: 123,
+    name: 'Essendon'
+  };
+  let team2 = {
+    id: 234,
+    name: 'Carlton'
+  };
+  model.homeTeam = team1;
+  model.awayTeam = team2;
+  model.winningTeam = team1;
+
+  assert.ok(model.get('didHomeTeamWin'));
+});
+
+test('it can check if the home team lost', function(assert) {
+  let model = this.subject();
+  let team1 = {
+    id: 123,
+    name: 'Essendon'
+  };
+  let team2 = {
+    id: 234,
+    name: 'Carlton'
+  };
+  model.homeTeam = team1;
+  model.awayTeam = team2;
+  model.winningTeam = team2;
+
+  assert.ok(!model.get('didHomeTeamWin'));
+});
+
+test('it can check if the away team won', function(assert) {
+  let model = this.subject();
+  let team1 = {
+    id: 123,
+    name: 'Essendon'
+  };
+  let team2 = {
+    id: 234,
+    name: 'Carlton'
+  };
+  model.homeTeam = team1;
+  model.awayTeam = team2;
+  model.winningTeam = team2;
+
+  assert.ok(model.get('didAwayTeamWin'));
+});
+
+test('it can check if the away team lost', function(assert) {
+  let model = this.subject();
+  let team1 = {
+    id: 123,
+    name: 'Essendon'
+  };
+  let team2 = {
+    id: 234,
+    name: 'Carlton'
+  };
+  model.homeTeam = team1;
+  model.awayTeam = team2;
+  model.winningTeam = team1;
+
+  assert.ok(!model.get('didAwayTeamWin'));
+});
+
+test('it can check if the game was a draw', function(assert) {
+  let model = this.subject();
+  model.winningTeam = null;
+
+  assert.ok(model.get('didDraw'));
+});
+
+test('it can format a pretty scoreline', function(assert) {
+  let model = this.subject();
+
+  assert.ok(model.prettyScore(10, 12, 72), '10.12 72');
+});
+
+test('it can format the home team\'s scoreline', function(assert) {
+  assert.expect(3);
+
+  let homeGoals = 10;
+  let homeBehinds = 12;
+  let homeTotal = 72;
+
+  let model = this.subject({
+    homeScoreGoals: homeGoals,
+    homeScoreBehinds: homeBehinds,
+    homeScoreTotal: homeTotal
+  });
+  model.prettyScore = (goals, behinds, total) => {
+    assert.equal(goals, homeGoals);
+    assert.equal(behinds, homeBehinds);
+    assert.equal(total, homeTotal);
+  };
+
+  model.get('prettyHomeScore');
+});
+
+test('it can format the away team\'s scoreline', function(assert) {
+  assert.expect(3);
+
+  let awayGoals = 10;
+  let awayBehinds = 12;
+  let awayTotal = 72;
+
+  let model = this.subject({
+    awayScoreGoals: awayGoals,
+    awayScoreBehinds: awayBehinds,
+    awayScoreTotal: awayTotal
+  });
+  model.prettyScore = (goals, behinds, total) => {
+    assert.equal(goals, awayGoals);
+    assert.equal(behinds, awayBehinds);
+    assert.equal(total, awayTotal);
+  };
+
+  model.get('prettyAwayScore');
 });
